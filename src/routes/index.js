b35d92e73e2127e664d98e3f677c2d23721fd7af
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-04-14 18:02:33 +0530

    Basic file structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.send('Tecnoesis-21')
-})
+});
+router.get('/hacks', (req, res) => res.render('../views/hacks.html'))
 
 module.exports = router
