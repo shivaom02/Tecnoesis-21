949abe5ad4d159da8924d23f4e73ea303f458bdd
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-04-19 14:06:09 +0530

    Menu js

--- public/js/hacks/hacks.js
+++ public/js/hacks/hacks.js
@@ -2,19 +2,21 @@ const navLinks = document.querySelectorAll('.nav-links')
 
 // scroll
 const hacksScroll = (sectionID) => {
-    if(sectionID === '#') {
+    hideMenu()
+
+    if (sectionID === '#') {
         window.scrollTo({
             top: 0,
             left: 0,
-            behavior: 'smooth'
-        });
+            behavior: 'smooth',
+        })
     } else {
-    let offSet = document.getElementById(sectionID).offsetTop;
-    window.scrollTo({
-        top: offSet,
-        left: 0,
-        behavior: 'smooth',
-    });
+        let offSet = document.getElementById(sectionID).offsetTop
+        window.scrollTo({
+            top: offSet,
+            left: 0,
+            behavior: 'smooth',
+        })
     }
 }
 
@@ -45,85 +47,85 @@ for (let index = 0; index < navLinks.length; index++) {
 }
 
 // events
-   const dayOneEvent = document.getElementById('day-one')
-   const dayTwoEvent = document.getElementById('day-two')
+const dayOneEvent = document.getElementById('day-one')
+const dayTwoEvent = document.getElementById('day-two')
 
-   const eventMoreDetails = document.getElementsByClassName(
-       'event-more-details'
-   )
-   const eventPopup = document.getElementsByClassName('event-popup')
-   const closePopupBtn = document.querySelectorAll('.close')
-   const overlay = document.querySelectorAll('.popup-overlay')
+const eventMoreDetails = document.getElementsByClassName('event-more-details')
+const eventPopup = document.getElementsByClassName('event-popup')
+const closePopupBtn = document.querySelectorAll('.close')
+const overlay = document.querySelectorAll('.popup-overlay')
 
-   dayOneEvent.addEventListener('click', () => {
-       dayOneEvent.classList.add('active')
-       dayTwoEvent.classList.remove('active')
+dayOneEvent.addEventListener('click', () => {
+    dayOneEvent.classList.add('active')
+    dayTwoEvent.classList.remove('active')
 
-       document.querySelector('.day-one-schedule').style.display = 'block'
-       document.querySelector('.day-two-schedule').style.display = 'none'
-   })
-   dayTwoEvent.addEventListener('click', () => {
-       dayOneEvent.classList.remove('active')
-       dayTwoEvent.classList.add('active')
+    document.querySelector('.day-one-schedule').style.display = 'block'
+    document.querySelector('.day-two-schedule').style.display = 'none'
+})
+dayTwoEvent.addEventListener('click', () => {
+    dayOneEvent.classList.remove('active')
+    dayTwoEvent.classList.add('active')
 
-       document.querySelector('.day-one-schedule').style.display = 'none'
-       document.querySelector('.day-two-schedule').style.display = 'block'
-   })
+    document.querySelector('.day-one-schedule').style.display = 'none'
+    document.querySelector('.day-two-schedule').style.display = 'block'
+})
 
-   for (let i = 0; i < eventMoreDetails.length; i++) {
-       eventMoreDetails[i].addEventListener('click', () => {
-           for (let j = 0; j < eventPopup.length; j++) {
-               if (j == i) {
-                   eventPopup[j].style.display = 'block'
-               } else {
-                   eventPopup[j].style.display = 'none'
-               }
-           }
-       })
-   }
+for (let i = 0; i < eventMoreDetails.length; i++) {
+    eventMoreDetails[i].addEventListener('click', () => {
+        for (let j = 0; j < eventPopup.length; j++) {
+            if (j == i) {
+                eventPopup[j].style.display = 'block'
+            } else {
+                eventPopup[j].style.display = 'none'
+            }
+        }
+    })
+}
 
-   const closePopup = () => {
-       for (let j = 0; j < eventPopup.length; j++) {
-           eventPopup[j].style.display = 'none'
-       }
-   }
-
-   closePopupBtn.forEach((el) => {
-       el.addEventListener('click', closePopup)
-   });
-   overlay.forEach((el) => {
-       el.addEventListener('click', closePopup)
-   });
-
-   // event menu
-    const closeMenuBtn = document.querySelector('.menu-close-btn');
-    const menuList = document.querySelector('.menu-list');
-    let menuFlag = 0;
-
-    function showMenu() {
-      menuFlag = 1;
-      menuList.style.transform = 'translate(-50%, -105%) scale(1)';
-      menuList.style.opacity = '1';
-      menuList.style.display = 'flex';
-      menuList.style.zIndex = '1000';
+const closePopup = () => {
+    for (let j = 0; j < eventPopup.length; j++) {
+        eventPopup[j].style.display = 'none'
     }
-    function hideMenu() {
-      menuFlag = 0;
-      menuList.style.opacity = '0';
-      menuList.style.transform = 'translate(-50%, -40%) scale(0)';
-      menuList.style.zIndex = '-10';
-    }
-    closeMenuBtn.addEventListener('click', () => {
-      menuFlag ? hideMenu() : showMenu();
-    });
+}
 
-    closeMenuBtn.addEventListener('mouseover', () => {
-      closeMenuBtn.style.transform = 'scale(1.1)';
-      closeMenuBtn.style.boxShadow = '0px 10px 10px rgba(0, 0, 0, .4)';
-    });
-    closeMenuBtn.addEventListener('mouseout', () => {
-      closeMenuBtn.style.transform = 'scale(1)';
-      closeMenuBtn.style.boxShadow = 'none';
-    });
+closePopupBtn.forEach((el) => {
+    el.addEventListener('click', closePopup)
+})
+overlay.forEach((el) => {
+    el.addEventListener('click', closePopup)
+})
 
-    hideMenu();
+// event menu
+const closeMenuBtn = document.querySelector('.menu-close-btn')
+const menuList = document.querySelector('.menu-list')
+let menuFlag = 0
+
+function showMenu() {
+    closeMenuBtn.textContent = 'CLOSE';
+    menuFlag = 1
+    menuList.style.transform = 'translate(-50%, -105%) scale(1)'
+    menuList.style.opacity = '1'
+    menuList.style.display = 'flex'
+    menuList.style.zIndex = '1000'
+}
+function hideMenu() {
+    closeMenuBtn.textContent = 'MENU';
+    menuFlag = 0
+    menuList.style.opacity = '0'
+    menuList.style.transform = 'translate(-50%, -40%) scale(0)'
+    menuList.style.zIndex = '-10'
+}
+closeMenuBtn.addEventListener('click', () => {
+    menuFlag ? hideMenu() : showMenu()
+})
+
+closeMenuBtn.addEventListener('mouseover', () => {
+    closeMenuBtn.style.transform = 'scale(1.1)'
+    closeMenuBtn.style.boxShadow = '0px 10px 10px rgba(0, 0, 0, .4)'
+})
+closeMenuBtn.addEventListener('mouseout', () => {
+    closeMenuBtn.style.transform = 'scale(1)'
+    closeMenuBtn.style.boxShadow = 'none'
+})
+
+hideMenu()
