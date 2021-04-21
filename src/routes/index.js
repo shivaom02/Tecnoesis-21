c9242862dc65d7ab9b0fdf8fb29dd573bdbdee5b
Trishanku <trishankusarma165@gmail.com>
2021-04-21 16:03:39 +0530

    normalised

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.render('welcome')
 })
 
+router.get('/hacks',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../../','views','hacks.html'));
+})
+
 router.use('/user', userRoutes)
 
 module.exports = router
