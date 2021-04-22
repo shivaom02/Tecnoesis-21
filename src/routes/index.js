b901e8b07d6a44a492ada8f7703268e59557e15c
sameer_99 <sameershrivastava46@gmail.com>
2021-04-22 13:16:28 +0530

    fixed hacks routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,13 @@ const express = require('express')
 const router = express.Router()
 const userRoutes = require('./user')
 const hacksRoutes = require('./hacks')
-const contactFormRoute = require('./contactForm');
 
 router.get('/', (req, res) => {
     res.render('welcome')
 })
 
-router.get('/hacks', (req, res) => {
-    res.sendFile(path.join(__dirname+'../../../views/hacks.html'));
-});
 
 router.use('/user', userRoutes)
 router.use('/hacks', hacksRoutes)
-router.use('/hacks/contactForm',contactFormRoute);
 
 module.exports = router
