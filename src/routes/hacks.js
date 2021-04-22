80a79b9e658bb4189130ea9067e49b61caf07fc3
sameer_99 <sameershrivastava46@gmail.com>
2021-04-22 21:10:55 +0530

    added contact form routes for landing pahe

--- src/routes/hacks.js
+++ src/routes/hacks.js
@@ -2,34 +2,31 @@ const express = require('express')
 const router = express.Router()
 const { ContactForm } = require('../models')
 
-router.get('/',(req,res)=>{
-    res.render('hacks');
+router.get('/', (req, res) => {
+    res.render('hacks')
 })
 
-router.post('/contactForm',async (req,res)=>{
+router.post('/contactForm', async (req, res) => {
     try {
-        
-        const { name , email , subject , message } = req.body;
+        const { name, email, subject, message } = req.body
 
         const newMessage = new ContactForm({
-            name, email , subject , message
+            name,
+            email,
+            subject,
+            message,
         })
-        
-        await newMessage.save();
 
-        console.log(newMessage);
+        await newMessage.save()
 
-        req.flash('success_msg', 'Message Sent');
+        req.flash('success_msg', 'Message Sent')
 
-        return res.redirect('/hacks');
-
+        return res.redirect('/hacks')
     } catch (error) {
-        
-        req.flash('error_msg', 'Some Error Occurred.Please Sent Again');
+        req.flash('error_msg', 'Some Error Occurred.Please Sent Again')
 
-        return res.redirect('/hacks');
+        return res.redirect('/hacks')
     }
 })
-
 
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
+
 const userRoutes = require('./user')
 const hacksRoutes = require('./hacks')
-
 const modulesRoutes = require('./modules')
+const tecnoContactFormRoute = require('./tecnoContactForm')
 
 router.get('/', (req, res) => {
     res.render('welcome')
 })
 
-
+router.use('/', tecnoContactFormRoute)
 router.use('/user', userRoutes)
 router.use('/hacks', hacksRoutes)
 router.use('/modules', modulesRoutes)
